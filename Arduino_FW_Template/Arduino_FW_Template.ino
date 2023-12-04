292f9451073c4a8e2cdfaaed36abd98b551543f2
lmtreser <lmtreser@gmail.com>
2023-12-04 14:07:56 -0300

    Plantilla Sketch Arduino

---
+++ Arduino_FW_Template/Arduino_FW_Template.ino
@@ -0,0 +1,44 @@
+/*
+    TITULO DEL PROYECTO
+    
+    Breve descripción del proyecto
+
+    Sobre el circuito: se recomienda enumerar los componentes conectados a cada 
+    entrada y a cada salida.
+
+    Bibliotecas de terceros: se recomienda adjuntar el código o url de cada biblioteca
+    de terceros utilizadas en el proyecto.
+
+    Más información sobre buenas prácticas:
+
+    * https://docs.arduino.cc/learn/contributions/arduino-writing-style-guide
+    * https://www.luisllamas.es/4-consejos-para-programar-codigo-mas-limpio-en-arduino/
+    * https://blog.wokwi.com/how-to-write-clean-arduino-code/
+
+    Creado dia mes año
+    Nombre del autor
+    Modificado dia mes año
+    Nombre del autor
+
+    https://url/of/online/tutorial.cc
+
+*/
+
+#include "pinout.h"
+
+// Instancia de objetos
+// .....
+
+// Variables globales
+// .....
+
+// Constantes
+// .....
+
+void setup() {
+
+}
+
+void loop() {
+
+}
